0abc412d0bf46ad43870db3df99018b6bae2c43e
JoshBayne <josh.bayne222@gmail.com>
2023-04-05 21:46:42 -0500

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   // set copy asset folder to dist
     eleventyConfig.addPassthroughCopy('assets');
   // set custom domain name
-    eleventyConfig.addPassthroughCopy("./CNAME");
+    eleventyConfig.addPassthroughCopy("CNAME");
   // set input and output folder
   return {
     dir: {
