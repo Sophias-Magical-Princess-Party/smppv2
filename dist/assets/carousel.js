4e9809bc845e7ab4a082aa89a467414484e18457
JoshBayne <josh.bayne222@gmail.com>
2024-01-21 22:16:39 -0600

    Update to add Popstar image and banner

--- dist/assets/carousel.js
+++ dist/assets/carousel.js
@@ -1,60 +1,62 @@
 var cont=0;
+var xx= setInterval(function(){
+    switch(cont)
+    {
+    case 0:{
+        $("#slider-1").fadeOut(400);
+        $("#slider-2").delay(400).fadeIn(400);
+        $("#sButton1").removeClass("bg-purple-800");
+        $("#sButton1").addClass("bg-purple-400");
+        $("#sButton2").addClass("bg-purple-800");
+        $("#sButton2").removeClass("bg-purple-400");
+        cont=1;
+        break;
+    }
+    case 1:{
+        $("#slider-2").fadeOut(400);
+        $("#slider-3").delay(400).fadeIn(400);
+        $("#sButton2").removeClass("bg-purple-800");
+        $("#sButton2").addClass("bg-purple-400");
+        $("#sButton3").addClass("bg-purple-800");
+        $("#sButton3").removeClass("bg-purple-400");
+        cont=2;
+        break;
+    }
+    case 2:{
+        $("#slider-3").fadeOut(400);
+        $("#slider-4").delay(400).fadeIn(400);
+        $("#sButton3").removeClass("bg-purple-800");
+        $("#sButton3").addClass("bg-purple-400");
+        $("#sButton4").addClass("bg-purple-800");
+        $("#sButton4").removeClass("bg-purple-400");
+        cont=3;
+        break;
+    }
+    case 3:{
+        $("#slider-4").fadeOut(400);
+        $("#slider-5").delay(400).fadeIn(400);
+        $("#sButton4").removeClass("bg-purple-800");
+        $("#sButton4").addClass("bg-purple-400");
+        $("#sButton5").addClass("bg-purple-800");
+        $("#sButton5").removeClass("bg-purple-400");
+        cont=4;
+        break;
+    }
+    case 4:{
+        $("#slider-5").fadeOut(400);
+        $("#slider-1").delay(400).fadeIn(400);
+        $("#sButton5").removeClass("bg-purple-800");
+        $("#sButton5").addClass("bg-purple-400");
+        $("#sButton1").addClass("bg-purple-800");
+        $("#sButton1").removeClass("bg-purple-400");
+        cont=0;
+        break;
+    }
+    
+    }},8000);
+
 function loopSlider(){
-  var xx= setInterval(function(){
-        switch(cont)
-        {
-        case 0:{
-            $("#slider-1").fadeOut(400);
-            $("#slider-2").delay(400).fadeIn(400);
-            $("#sButton1").removeClass("bg-purple-800");
-            $("#sButton1").addClass("bg-purple-400");
-            $("#sButton2").addClass("bg-purple-800");
-            $("#sButton2").removeClass("bg-purple-400");
-            cont=1;
-            break;
-        }
-        case 1:{
-            $("#slider-2").fadeOut(400);
-            $("#slider-3").delay(400).fadeIn(400);
-            $("#sButton2").removeClass("bg-purple-800");
-            $("#sButton2").addClass("bg-purple-400");
-            $("#sButton3").addClass("bg-purple-800");
-            $("#sButton3").removeClass("bg-purple-400");
-            cont=2;
-            break;
-        }
-        case 2:{
-            $("#slider-3").fadeOut(400);
-            $("#slider-4").delay(400).fadeIn(400);
-            $("#sButton3").removeClass("bg-purple-800");
-            $("#sButton3").addClass("bg-purple-400");
-            $("#sButton4").addClass("bg-purple-800");
-            $("#sButton4").removeClass("bg-purple-400");
-            cont=3;
-            break;
-        }
-        case 3:{
-            $("#slider-4").fadeOut(400);
-            $("#slider-5").delay(400).fadeIn(400);
-            $("#sButton4").removeClass("bg-purple-800");
-            $("#sButton4").addClass("bg-purple-400");
-            $("#sButton5").addClass("bg-purple-800");
-            $("#sButton5").removeClass("bg-purple-400");
-            cont=4;
-            break;
-        }
-        case 4:{
-            $("#slider-5").fadeOut(400);
-            $("#slider-1").delay(400).fadeIn(400);
-            $("#sButton5").removeClass("bg-purple-800");
-            $("#sButton5").addClass("bg-purple-400");
-            $("#sButton1").addClass("bg-purple-800");
-            $("#sButton1").removeClass("bg-purple-400");
-            cont=0;
-            break;
-        }
-        
-        }},8000);
+  xx;
 }
 
 function reinitLoop(time){
