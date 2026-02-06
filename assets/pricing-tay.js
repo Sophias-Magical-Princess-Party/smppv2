ac5a005d4d5edfa9b3176fc3ddae050e549ce18b
JoshBayne <josh.bayne222@gmail.com>
2026-02-06 09:51:51 -0600

    Update price for 'one' time selection

--- assets/pricing-tay.js
+++ assets/pricing-tay.js
@@ -1,11 +1,12 @@
 $("select").change(function() {
     if($('#time').val() == "one") 
     {
-        $("#price-imp").text("$225");
+        $("#price-imp").text("$250");
     }
     if($('#time').val() == "two") 
     {
         $("#price-imp").text("$450");
     }
 });
+
 
